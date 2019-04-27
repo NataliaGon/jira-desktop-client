a22635dfdd264788306b466a107f62c3ee34f5e4
Natalia Goncharova <natalia.g@morning.agency>
2019-04-27 13:15:24 +0300

    check issues

--- src/reducers/pinReducer.ts
+++ src/reducers/pinReducer.ts
@@ -8,9 +8,16 @@ export default function pinReducer(state = initialState, action?:any) {
   const issue = action.issue
   switch (action.type) {
     case TO_PIN:
+
+      function checkIndex(item:any){
+       return  issue.id != item.id
+      }
+     if(state.pin.every(checkIndex)){
       return {
         pin:[...state.pin, issue ]
       }
+     }
+     
     default:
       return state;
   }
