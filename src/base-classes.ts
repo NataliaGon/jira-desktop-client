f4c297b2f9f471a1048b3af1475871a963a932e5
Natalia Goncharova <natalia.g@morning.agency>
2019-04-27 12:43:24 +0300

    base classes

--- src/base-classes.ts
+++ src/base-classes.ts
@@ -1,24 +1,6 @@
 import * as React from "react";
 
-export type ComponentBaseSkin = "default";
-export type TextSize = "large" | "medium" | "regular" | "small";
-export type TextColor = "black" | "grey" | "blue" | "white";
-export type IconColor = TextColor | "";
-export type FontFamily = "regular" | "serif" | "icons";
-
-export class DefaultTextProperties {
-    textSize: TextSize = "regular";
-    textColor: TextColor = "grey";
-    fontFamily: FontFamily = "regular";
-}
-
 export class ComponentBaseProperties {
-    componentClasses?: string[] | string;
-    text?: string;
-    textSize?: TextSize;
-    textColor?: TextColor;
-    fontFamily?: FontFamily;
-    draggable?: boolean;
 }
 
 export class ComponentBaseState {
@@ -33,18 +15,4 @@ export class ComponentBase<ComponentBaseProperties, ComponentBaseState> extends 
     focusRef: {
         focus: () => void;
     } | undefined;
-
-    protected defaultProperties = function (): any {
-        return new DefaultTextProperties();
-    }
-}
-
-export class ComponentAnimatedProperties extends ComponentBaseProperties {
-    transitionName?: string;
-    transitionEnterTimeout?: number;
-    transitionLeaveTimeout?: number;
-}
-
-export class ActiveProperty{
-    active:boolean
 }
